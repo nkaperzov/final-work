993bd74105dd5b9ab0f3e776c21057b0b9685e50
nkaperzov <47456911+nkaperzov@users.noreply.github.com>
2019-03-25 18:26:33 +0200

    updated

--- chat.js
+++ chat.js
@@ -1,15 +1,35 @@
 function input() {
+
     var text = document.getElementById('message').value;
     var name = document.getElementById('username').value;
 
     var now = new Date();
     var timenow = now.getHours() + ':' + now.getMinutes();
 
-    var nicktime = name + ' ' + timenow;
 
 
 
+    var div = document.createElement("div");
+    div.className = ("container");
+    div.id = ("msgcontent");
+    var uname = document.createElement("h6");
+    var p = document.createElement("p");
+    var timebox = document.createElement("span");
+    timebox.className = ("time-right")
 
+    uname.innerHTML = name;
+    p.innerHTML = text;
+    timebox.innerHTML = timenow;
+
+
+
+    if (!text == '' && !name == '') {
+        var block = document.getElementById("msglist").appendChild(div);
+        block.appendChild(uname);
+        block.appendChild(p);
+        block.appendChild(timebox);
+
+    }
 
     //add TimeStamp
     //add css layout for messages
