a09a1a5568b47da84db487ec92409bb4d386a391
Nikita Kaperzov <nikita.kaperzovs@gmail.com>
2019-03-25 15:11:09 +0200

    Initial commit

---
+++ chat.js
@@ -0,0 +1,17 @@
+function input() {
+    var text = document.getElementById('message').value;
+    var name = document.getElementById('username').value;
+
+    var now = new Date();
+    var timenow = now.getHours() + ':' + now.getMinutes();
+
+    var nicktime = name + ' ' + timenow;
+
+
+
+
+
+    //add TimeStamp
+    //add css layout for messages
+    //output in message area
+}
